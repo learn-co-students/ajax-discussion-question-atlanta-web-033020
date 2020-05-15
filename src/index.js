8eec2c885a765008971cb3ce1673b189f8c428c9
rpdecks <rpdecks@gmail.com>
2020-05-15 09:32:42 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,29 @@
-const fullname = document.getElementById("fullname");
-console.log("CONTENT NOT YET LOADED!", fullname); //what will fullname evaluate to?
 
-document.addEventListener("DOMContentLoaded", () => {
-  console.log("CONTENT LOADED!");
-});
+// const fullname = document.getElementById("fullname");
+// console.log("CONTENT NOT YET LOADED!", fullname); //what will fullname evaluate to?
+
+// document.addEventListener("DOMContentLoaded", () => {
+//   console.log("CONTENT LOADED!");
+// });
+
+function fetchData() {
+  fetch('https://randomuser.me/api/')
+    .then(res => res.json())
+    .then(res => sendDataToBrowser(res.results[0]))
+}
+
+fetchData();
+
+const fullname = document.querySelector('#fullname');
+
+function sendDataToBrowser(data) {
+  fullname.innerHTML = `${data.name.first} ${data.name.last}`;
+  email.innerHTML = `${data.email}`;
+  street.innerHTML = `${data.location.street.number}  ${data.location.street.name}`;
+  city.innerHTML = `${data.location.city}`;
+  state.innerHTML = `${data.location.state}`;
+  postcode.innerHTML = `${data.location.postcode}`;
+  phone.innerHTML = `${data.phone}`
+  cell.innerHTML = `${data.cell}`
+  date_of_birth.innerHTML = `${data.dob.date.split('T')[0]}`
+}
